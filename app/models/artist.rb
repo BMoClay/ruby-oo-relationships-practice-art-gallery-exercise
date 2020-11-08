07fd219596678ba7be9bb33153fad5e17fcd0f6e
BMoClay <benmorgancleveland@gmail.com>
2020-11-07 22:34:45 -0500

    first commit

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,43 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.map{|painting| painting.gallery}   
+  end
+
+  def cities
+    self.galleries.map{|gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    Artist.all.sum{|artist| artist.years_experience}
+  end
+
+  def proficiency
+   self.paintings.count.to_f / self.years_experience
+  end
+
+  def self.most_prolific
+    Artist.all.max_by{|artist| artist.proficiency}
+  end
+
+  def create_painting(title, price, gallery)
+   Painting.new("#{title}", "#{price}", self, "#{gallery}")
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,17 @@
 require_relative '../config/environment.rb'
 
+ben = Artist.new("Ben", 1)
+maggie = Artist.new("Maggie", 9)
 
+fra = Gallery.new("FRA", "New York")
+buckets = Gallery.new("Galerie Buckets", "Cologne")
+
+shark_tonk_2 = Painting.new("Shark Tonk II", 5000, ben, fra)
+shark_tonk = Painting.new("Shark Tonk", 5000, ben, fra)
+heart_fountain = Painting.new("Heart Fountain", 10000, maggie, buckets)
+unhappy_break_up = Painting.new("Unhappy Break Up", 12000, maggie, buckets)
+unhappy_break_up_2 = Painting.new("Unhappy Break Up II", 12000, maggie, fra)
+tile = Painting.new("Tile", 6000, maggie, buckets)
 
 
 binding.pry
